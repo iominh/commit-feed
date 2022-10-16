a8894f330e02311983e7632aa697e846f9e331d1
Minh Nguyen <minh.909.nguyen@gmail.com>
2022-10-15 18:22:55 -0700

    Test with another variable

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,7 +1,7 @@
 describe("Home Page", () => {
   it("displays home page", () => {
-    expect(Cypress.env('CYPRESS_BASE_URL')).to.not.equal(null);
-    expect(Cypress.env('CYPRESS_BASE_URL')).to.not.equal('');
-    cy.visit(Cypress.env('CYPRESS_BASE_URL'));
+    expect(Cypress.env('BASE_URL')).to.not.equal(null);
+    expect(Cypress.env('BASE_URL')).to.not.equal('');
+    cy.visit(Cypress.env('BASE_URL'));
   });
 });
