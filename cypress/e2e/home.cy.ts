1c5e7ed1b8b3229b157ffcba7d05e4b4588094ac
Minh Nguyen <minh.909.nguyen@gmail.com>
2022-10-15 18:06:28 -0700

    Try BASE_URL

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,6 +1,7 @@
 describe("Home Page", () => {
   it("displays home page", () => {
     console.log('Crypress_base_url', Cypress.env('CYPRESS_BASE_URL'));
-    cy.visit(Cypress.env('CYPRESS_BASE_URL'));
+    console.log('BASE_URL', Cypress.env('BASE_URL'));
+    cy.visit(Cypress.env('BASE_URL'));
   });
 });
