b3353bb5c6bd7ecdde1efef3cb659a93d760b311
Minh Nguyen <minh.909.nguyen@gmail.com>
2022-10-16 02:02:36 -0700

    boom we have data!

--- src/pages/RepoPage/RepoPage.tsx
+++ src/pages/RepoPage/RepoPage.tsx
@@ -1,19 +1,31 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link, useLocation, useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 
 function RepoPage() {
   let location = useLocation();
-  let { id, repo } = useParams();
+  let { id, user } = useParams();
+  const [commits, setCommits] = useState([]);
 
   useEffect(() => {
     console.log("repo page", location);
-    document.title = `${location.pathname ?? ''} - Commit Feed`;
+    document.title = `${location.pathname ?? ""} - Commit Feed`;
   }, [location]);
+
+  const { error, data } = useQuery(["commits"], () => {
+    return fetch(`https://api.github.com/repos/${user}/${id}/commits?page=1`)
+      .then((res) => res.json())
+      .then((res) => {
+        setCommits(res);
+        return res;
+      });
+  });
 
   return (
     <div>
       <h1>Repo page</h1>
       <Link to="/">Go Back</Link>
+      <div>{JSON.stringify(commits, null, 2)}</div>
     </div>
   );
 }
