f4cdf76a1d974ec41fd15c2a6543b6c11dc26bc9
Minh Nguyen <minh.909.nguyen@gmail.com>
2022-10-15 17:33:38 -0700

    Remove env variable

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,12 +2,6 @@ import { defineConfig } from "cypress";
 import customViteConfig from "./vite.config";
 
 export default defineConfig({
-  // Specifies local environment variables. See .github directory
-  // for other environments
-  env: {
-    CYPRESS_BASE_URL: 'http://127.0.0.1:5173/'
-  },
-
   component: {
     devServer: {
       framework: "react",
