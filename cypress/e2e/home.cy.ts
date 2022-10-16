7897c53c09497d436b0c3459505bb6dd00ce0c1f
Minh Nguyen <minh.909.nguyen@gmail.com>
2022-10-15 18:28:15 -0700

    Env var test

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,5 +1,7 @@
 describe("Home Page", () => {
   it("displays home page", () => {
+    expect(Cypress.env('CYPRESS_BASE_URL')).to.not.equal(null);
+    expect(Cypress.env('CYPRESS_BASE_URL')).to.not.equal('');
     cy.visit('http://127.0.0.1:4173');
   });
 });
