c04c0a86328b246fb8ae27ddf7dbdb0638f8cd1b
Minh Nguyen <minh.909.nguyen@gmail.com>
2022-10-16 20:56:12 -0700

    Add more tests and tab plugin

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -6,4 +6,14 @@ describe("Home Page", () => {
   it("changes theme", () => {
     cy.testTheme()
   });
+
+  it("displays user dropdown on input", () => {
+    cy.visit(Cypress.env("BASE_URL"));
+
+    // find the second input, type text to trigger auto-suggest and press down
+    cy.get('input').eq(1).should('be.visible').type('EbookFoundation');
+    cy.wait(500);
+    cy.get('input').eq(1).type('{enter}');
+
+  });
 });
