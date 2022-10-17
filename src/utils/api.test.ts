5b939a1efbad19acbfa20845b4c11c67a4b836d1
Minh Nguyen <minh.909.nguyen@gmail.com>
2022-10-16 18:36:49 -0700

    More test fixes

--- src/utils/api.test.ts
+++ src/utils/api.test.ts
@@ -1,15 +1,26 @@
 import { describe, expect } from "@jest/globals";
-import { getUsers } from "./api";
+import { getUsers, getRepos, getCommits } from "./api";
 
 describe("API Utils", () => {
   let users: any = null;
+  let repos: any = null;
+  let commits: any = null;
 
   beforeEach(async () => {
     users = await getUsers("user1");
+    repos = await getRepos("user1");
+    commits = await getCommits("user1", "reo1");
   });
 
   it("getUsers not null", async () => {
     expect(users).not.toBe(null);
-    console.log(users);
+  });
+
+  it("getRepos not null", async () => {
+    expect(repos).not.toBe(null);
+  });
+
+  it("getCommits not null", async () => {
+    expect(commits).not.toBe(null);
   });
 });
