a27635497ad98edcd17bb42a909f764337be6d91
Minh Nguyen <minh.909.nguyen@gmail.com>
2022-10-16 20:09:48 -0700

    Try using BASE_URL

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,10 +1,6 @@
 describe("Home Page", () => {
-  before(() => {
-    expect(Cypress.env('CYPRESS_BASE_URL')).to.not.equal(null);
-    expect(Cypress.env('CYPRESS_BASE_URL')).to.not.equal('');
-  })
 
   it("displays home page", () => {
-    cy.visit('http://127.0.0.1:4173');
+    cy.visit(Cypress.env('BASE_URL'));
   });
 });
