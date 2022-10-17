dee0c8ce19abc06a61d6783b8a244cb3d5fdcba2
Minh Nguyen <minh.909.nguyen@gmail.com>
2022-10-16 18:03:02 -0700

    WIP tests

--- src/pages/ErrorPage/ErrorPage.test.tsx
+++ src/pages/ErrorPage/ErrorPage.test.tsx
@@ -1,19 +1,17 @@
 import { describe, expect, test } from "@jest/globals";
 import ErrorPage from "./ErrorPage";
-import renderer from 'react-test-renderer';
-import {BrowserRouter, MemoryRouter} from 'react-router-dom'
-import { createMemoryHistory } from 'history';
-const { render } = require("@testing-library/react");
+import { createMemoryHistory } from "history";
+import { fireEvent, render } from "@testing-library/react";
 const { Router } = require("react-router-dom");
 
 describe("ErrorPage", () => {
-  it('renders component', () => {
-    const history = createMemoryHistory({ initialEntries: ['/home'] });
-    const { getByText } = render(
+  it("renders component", () => {
+    const history = createMemoryHistory({ initialEntries: ["/error"] });
+    render(
       <Router location={history.location} navigator={history}>
         <ErrorPage></ErrorPage>
       </Router>
     );
-    expect(history.location.pathname).toBe('/home');
+    expect(history.location.pathname).toBe("/error");
   });
 });
